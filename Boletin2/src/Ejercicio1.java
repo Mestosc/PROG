cc652671a358bc9eb758d2acad9d9a4f48be09bd
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-16 21:11:19 +0200

    Documentacion del primer ejercicio

--- Boletin2/src/Ejercicio1.java
+++ Boletin2/src/Ejercicio1.java
@@ -1,8 +1,15 @@
-
+/**
+ * Calculo del area de un triangulo como se pedia de valores especificos, que fueron especificados en el ejercicio
+ * simplemente utilice la informacion ahi especificada.
+ * @author Oscar Rodriguez
+ * @version 1.0
+ * **/
 public class Ejercicio1 {
     public static void main(String[] args) {
+        // Declaramos las variables que contienen los valores
         int base = 4;
         int altura = 3;
+        // Declaramos una variable donde se guarda el codigo
         int area = base * altura / 2;
         System.out.println("El area de este triangulo es: " + area + "m²");
     }
