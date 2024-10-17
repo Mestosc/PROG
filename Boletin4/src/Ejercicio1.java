ca482ba54ecf50183bd2e0b37cd6ea05661ae4eb
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-17 13:43:06 +0200

    Empezando programa

---
+++ Boletin4/src/Ejercicio1.java
@@ -0,0 +1,11 @@
+/**
+ * Este programa nos mostrara si un articulo es de primera necesidad a partir del nombre
+ * y del numero de ventas
+ * @author Oscar Rodriguez
+ * @version 1.0
+ * **/
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+    }
+}
