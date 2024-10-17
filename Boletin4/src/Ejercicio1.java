4befb6b4bfda2f703afc51ebd17219f9103ed304
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-17 14:02:05 +0200

    Acabando de documentar el codigo

--- Boletin4/src/Ejercicio1.java
+++ Boletin4/src/Ejercicio1.java
@@ -11,21 +11,40 @@ public class Ejercicio1 {
         // Primero inicializamos las variables donde se guardara la informacion
         int numeroVentas;
         String nombreProducto;
+
+        // Preparamos el Scanner para recojer informacion
         Scanner informacion = new Scanner(System.in);
-        // Indicar que
+
+        // Indicar que se quiere solicitar el nombre del producto
         System.out.print("Digame el nombre del producto:");
+
+        // Solicitamos la informacion del nombre del prodcuto
         nombreProducto = informacion.next();
+
+        // Solicitamos informacion del numero de ventas del producto
         System.out.print("Digame cuantas ventas ha tenido el producto:");
+
+        // Solicitamos la informacion del numero de ventas
         numeroVentas = informacion.nextInt();
+
+        // Comprobamos si las ventas son menores a 100, e imprimimos que es de consumo bajo
         if (numeroVentas<=100) {
             System.out.println("El producto " + nombreProducto + " ha tenido " + numeroVentas + " ventas" + " es un articulo de consumo bajo");
         }
+
+        /* Como es un else if ya sabemos que la primera no se cumple asi que solo necesitamos
+        * saber si es menor o igual a 500, imprimimos que es de Consumo medio */
         else if (numeroVentas<=500) {
             System.out.println("El producto " + nombreProducto + " ha tenido " + numeroVentas + " ventas" + " es un articulo de consumo medio");
         }
+
+        /* Como es un else if ya sabemos que la segunda no se ha cumplido asi que solo necesitamos
+         * saber si es menor o igual a 1000, y imprimimos el mensaje de que el producto es de Consumo Alto */
         else if (numeroVentas<=1000) {
             System.out.println("El producto " + nombreProducto + " ha tenido " + numeroVentas + " ventas" + " es un articulo de consumo alto");
         }
+
+        // Aqui solo necesitamos saber que es mayor que 1000, e imprimimos el mensaje de que es de Primera necesidad
         else if (numeroVentas>1000) {
             System.out.println("El producto " + nombreProducto + " ha tenido " + numeroVentas + " ventas" + " es un articulo de primera necesidad");
         }
