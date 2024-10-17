79ce1612993379783fadcd334c7b5bb82c723899
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-17 16:41:34 +0200

    Implementando introduccion de datos para obtener DNI

--- Boletin4/src/Ejercicio5.java
+++ Boletin4/src/Ejercicio5.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class Ejercicio5 {
     public static void main(String[] args) {
         Scanner numero = new Scanner(System.in);
+        System.out.println("Introduzca su numero de DNI:");
+        int DNI = numero.nextInt();
     }
 }
