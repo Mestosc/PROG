7ca32cb09da5ac9bc2f70d3ee1ea870185de32a8
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-17 13:52:45 +0200

    Los condicionales para las tres primeras situaciones

--- Boletin4/src/Ejercicio1.java
+++ Boletin4/src/Ejercicio1.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Este programa nos mostrara si un articulo es de primera necesidad a partir del nombre
  * y del numero de ventas
@@ -6,6 +8,23 @@
  * **/
 public class Ejercicio1 {
     public static void main(String[] args) {
+        // Primero inicializamos las variables donde se guardara la informacion
+        int numeroVentas;
+        String nombreProducto;
+        Scanner informacion = new Scanner(System.in);
+        System.out.print("Digame el nombre del producto:");
+        nombreProducto = informacion.next();
+        System.out.print("Digame cuantas ventas ha tenido el producto:");
+        numeroVentas = informacion.nextInt();
+        if (numeroVentas<=100) {
+            System.out.println("El producto " + nombreProducto + " ha tenido " + numeroVentas + " ventas" + " es un articulo de consumo bajo");
+        }
+        else if (100>numeroVentas && numeroVentas<=500) {
+            System.out.println("El producto " + nombreProducto + " ha tenido " + numeroVentas + " ventas" + " es un articulo de consumo medio");
+        }
+        else if (numeroVentas>500 & numeroVentas<=1000) {
+            System.out.println("El producto " + nombreProducto + " ha tenido " + numeroVentas + " ventas" + " es un articulo de consumo alto");
+        }
 
     }
 }
