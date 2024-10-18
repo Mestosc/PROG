981e50a6ce305e4fdac6febef165283193b74fec
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-18 09:09:22 +0200

    Añadiendo documentacion de clase

--- Boletin2_extra/src/Ejercicio1.java
+++ Boletin2_extra/src/Ejercicio1.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 
+/**
+ * Introduciendo el precio de un producto sin descuento, y el dinero que se le descuenta nos permite sacar
+ * el porcentaje del descuento
+ * @author Oscar Rodriguez
+ * @version 1.0
+ * **/
 public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner datos = new Scanner(System.in);
