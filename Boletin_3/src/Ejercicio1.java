a2cd82155c05a55a6ea0c633a663d897c285158f
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-18 09:20:53 +0200

    Comentando un poco el ejercicio

--- Boletin_3/src/Ejercicio1.java
+++ Boletin_3/src/Ejercicio1.java
@@ -12,6 +12,8 @@ public class Ejercicio1 {
         System.out.print("Introduzca un valor numerico:");
         valTeclado = introduccionValor.nextInt();
         introduccionValor.close();
+
+        // Comprobamos si el valor es mayor que 0 si asi es lo mostrara y dira que es positivo
         if (valTeclado > 0) {
             System.out.println(valTeclado);
             System.out.println("Es positivo");
