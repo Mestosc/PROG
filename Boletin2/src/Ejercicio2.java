10d01eabecefbfc8c89362d4fd21c2f38cf792a4
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-18 09:14:24 +0200

    Añadiendo documentacion de clase al programa

--- Boletin2/src/Ejercicio2.java
+++ Boletin2/src/Ejercicio2.java
@@ -1,4 +1,9 @@
 public class Ejercicio2 {
+    /**
+     * Calcula el area de un cuadrado de forma facil
+     * @author Oscar Rodriguez
+     * @version 1.0
+     * **/
     public static void main(String [] args) {
         int lado;
         lado = 3;
