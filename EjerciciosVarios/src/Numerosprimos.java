2a45800d447a0404c893060c2808527f9b39db7c
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-23 09:51:35 +0200

    Averiguando los primos

--- EjerciciosVarios/src/Numerosprimos.java
+++ EjerciciosVarios/src/Numerosprimos.java
@@ -4,6 +4,8 @@ public class Numerosprimos {
     public static void main(String[] args) {
         Scanner datos = new Scanner(System.in);
         int numero = datos.nextInt();
+        for (int num = 2; num<=numero;num++) {
+            boolean esPrimo = numero % num != 0;
 
         }
     }
