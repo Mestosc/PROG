2f71de81ec4e45e140ae01a2f7e58540898256e0
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-23 19:11:44 +0200

    Tras debugeo se confirmo que segun flujo de datos funciona mejor

--- EjerciciosVarios/src/SolicitarNota.java
+++ EjerciciosVarios/src/SolicitarNota.java
@@ -4,23 +4,25 @@ public class SolicitarNota {
     public static void main(String[] args) {
 
         //Declaramos las variables de la nota, la suma de las varias notas y por ultimo la media de las varias notas
-        int nota = 0, sumaNotas = 0, cantidadAlumnos = 1, media;
+        int nota, sumaNotas = 0, cantidadAlumnos = 0, media;
 
         // Declaramos Scanner
         Scanner input = new Scanner(System.in);
         System.out.print("Indicame la nota que tiene cada alumno se sale con -1: ");
-        while (nota!=-1) {
 
-            // Para poder actualizar la cantidad desde el bucle necesitamos tener una seccion de entrada que referencie a notas dentro del mismo
-            nota = input.nextInt();
+        nota = input.nextInt();
+
+        while (nota!=-1) {
 
             // Igualamos la suma a la nota más si misma esto hara que cuando añadamos más notas se nos acumule
             sumaNotas = nota + sumaNotas;
 
-            /*Incrementamos la cantidad de Alumnos cada vez que añadimos la nota de un alumno la cantidad de Alumnos con la que se trabaja es distinta
-            * más concretamente si añadimos más notas se refiere a más alumnos*/
-            cantidadAlumnos++;
+            // Para poder actualizar la cantidad desde el bucle necesitamos tener una seccion de entrada que referencie a notas dentro del mismo
+            nota = input.nextInt();
 
+            /*Incrementamos la cantidad de Alumnos cada vez que añadimos la nota de un alumno la cantidad de Alumnos con la que se trabaja es distinta
+             * más concretamente si añadimos más notas se refiere a más alumnos*/
+            cantidadAlumnos++;
 
         }
         media = sumaNotas/cantidadAlumnos;
