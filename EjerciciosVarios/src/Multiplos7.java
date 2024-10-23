90dbf9a0f21f7cf95ad37bc6ffdd11821c98085d
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-23 09:15:26 +0200

    Implementado ejercicio para sacar multiplos de 7

---
+++ EjerciciosVarios/src/Multiplos7.java
@@ -0,0 +1,9 @@
+public class Multiplos7 {
+    public static void main(String[] args) {
+
+        for (int numero = 1;numero<=100/7; numero++) {
+            int multiplo = numero*7;
+            System.out.println("Los multiplos de siete menores que 100 son:"  + multiplo);
+        }
+    }
+}
