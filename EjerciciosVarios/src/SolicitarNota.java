6c9e20bc2ae172f3a7f4f921bc645f5a2c0d735c
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-23 15:38:48 +0200

    Haciendo que el ejercicio de Solicitar Nota funcione y comentado

--- EjerciciosVarios/src/SolicitarNota.java
+++ EjerciciosVarios/src/SolicitarNota.java
@@ -2,14 +2,25 @@ import java.util.Scanner;
 
 public class SolicitarNota {
     public static void main(String[] args) {
-        int nota3;
-        Scanner preguntar = new Scanner(System.in);
-        int nota = preguntar.nextInt();
-        while (nota!=-1) {
-            int nota2 = nota;
-            nota= preguntar.nextInt();
-            nota3 = (nota + nota2);
-        }
 
-    }
-}
+        //Declaramos las variables de la nota, la suma de las varias notas y por ultimo la media de las varias notas
+        int nota, sumaNotas = 0, cantidadAlumnos = 0, media;
+
+        // Declaramos Scanner
+        Scanner input = new Scanner(System.in);
+        System.out.print("Indicame la nota que tiene cada alumno se sale con -1: ");
+        nota = input.nextInt();
+        while (nota!=-1) {
+            // Igualamos la suma a la nota más si misma esto hara que cuando añadamos más notas se nos acumule
+            sumaNotas = nota + sumaNotas;
+
+            // Para poder actualizar la cantidad desde el bucle necesitamos tener una seccion de entrada que referencie a notas dentro del mismo
+            nota = input.nextInt();
+
+            /*Incrementamos la cantidad de Alumnos cada vez que añadimos la nota de un alumno la cantidad de Alumnos con la que se trabaja es distinta
+             * más concretamente si añadimos más notas se refiere a más alumnos*/
+            cantidadAlumnos++;
+        }
+        media = sumaNotas/cantidadAlumnos;
+        System.out.println("La media de notas de los alumnos es " + media);
+    }}
