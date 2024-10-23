c1ee6619cc48cdcc1efb897f0d74411c4c7e0b88
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-23 09:30:05 +0200

    Eliminacion de variable innecesaria y añadiendo Ejercicio numeros primos

--- EjerciciosVarios/src/Multiplos7.java
+++ EjerciciosVarios/src/Multiplos7.java
@@ -2,8 +2,7 @@ public class Multiplos7 {
     public static void main(String[] args) {
 
         for (int numero = 1;numero<=100/7; numero++) {
-            int multiplo = numero*7;
-            System.out.println("Los multiplos de siete menores que 100 son:"  + multiplo);
+            System.out.println("Los multiplos de siete menores que 100 son:"  + numero*7);
         }
     }
 }
---
+++ EjerciciosVarios/src/Numerosprimos.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Numerosprimos {
+    public static void main(String[] args) {
+        Scanner datos = new Scanner(System.in);
+        int numero = datos.nextInt();
+        for (int primo = 1;numero<=1;numero++) {
+
+        }
+    }
+}
