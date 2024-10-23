4c4861a743b182141e29d944f80d100897267fc7
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-23 13:23:27 +0200

    Implementando funcionalidad de forma experimental

--- EjerciciosVarios/src/SolicitarNota.java
+++ EjerciciosVarios/src/SolicitarNota.java
@@ -3,15 +3,11 @@ import java.util.Scanner;
 public class SolicitarNota {
     public static void main(String[] args) {
         Scanner preguntar = new Scanner(System.in);
-
-        while (true) {
-            int nota = preguntar.nextInt();
-            if (nota==-1) {
-                break;
-            }
-            else {
-                System.out.println(nota+(nota*30)/30);
-            }
+        int nota = preguntar.nextInt();
+        while (nota!=-1) {
+            int nota2 = nota;
+            nota= preguntar.nextInt();
+            nota = nota + nota2;
         }
 
     }
