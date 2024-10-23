03f0aeb5cf8c9204bfabde0baa37d24bf323a9fc
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-23 09:49:13 +0200

    Replanteando

--- EjerciciosVarios/src/Numerosprimos.java
+++ EjerciciosVarios/src/Numerosprimos.java
@@ -4,11 +4,7 @@ public class Numerosprimos {
     public static void main(String[] args) {
         Scanner datos = new Scanner(System.in);
         int numero = datos.nextInt();
-        for (int num = 1; num<=numero;num++) {
-            boolean esPrimo = num % 2 != 0;
-            if (esPrimo) {
-                System.out.println(num);
-            }
+
         }
     }
 }
