4f642688f6cc37d516ece44c022af6e4652f26bc
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-23 13:10:19 +0200

    Añadiendo el limite en cantidad a una variable y otras cosas

--- EjerciciosVarios/src/Multiplos7.java
+++ EjerciciosVarios/src/Multiplos7.java
@@ -6,12 +6,15 @@
 public class Multiplos7 {
     public static void main(String[] args) {
 
-        // Numero del que sacamos multiplos
+        // Numero del que sacamos multiplos asi si deseas establecer otro mediante un Scanner se puede
         int multiplosDe = 7;
+
+        // Limite en cantidad asi si quieres poner otro limite a traves de un Scanner
+        int limite = 100;
         /* Al dividir 100/numero usando enteros
         se consigue saber el numero por el que se puede multiplicar ese numero sin que sobrepase el 100*/
 
-        for (int numero = 1;numero<=100/multiplosDe; numero++) {
+        for (int numero = 1;numero<=limite/multiplosDe; numero++) {
             System.out.println("Los multiplos de " + multiplosDe + " menores que 100 son:"  + numero*multiplosDe);
         }
     }
