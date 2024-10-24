3ffec98d8f445cce880b1910a6b81edc9487b84f
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-24 13:32:44 +0200

    Otra solucion

--- EjerciciosVarios/src/SolicitarNota.java
+++ EjerciciosVarios/src/SolicitarNota.java
@@ -4,7 +4,7 @@ public class SolicitarNota {
     public static void main(String[] args) {
 
         //Declaramos las variables de la nota, la suma de las varias notas y por ultimo la media de las varias notas
-        int nota = 0, sumaNotas = 0, cantidadAlumnos = 0, media;
+        double nota = 0, sumaNotas = 0, cantidadAlumnos = 0, media;
 
         // Declaramos Scanner
         Scanner input = new Scanner(System.in);
@@ -13,18 +13,19 @@ public class SolicitarNota {
         do {
             System.out.print("Indicame la nota que tiene cada alumno se sale con -1: ");
 
-            // Igualamos la suma a la nota más si misma esto hara que cuando añadamos más notas se nos acumule
-            sumaNotas = nota + sumaNotas;
+            // Para poder actualizar la cantidad desde el bucle necesitamos tener una seccion de entrada que referencie a notas dentro del mismo
+            nota = input.nextDouble();
 
             /*Incrementamos la cantidad de Alumnos cada vez que añadimos la nota de un alumno la cantidad de Alumnos con la que se trabaja es distinta
              * más concretamente si añadimos más notas se refiere a más alumnos*/
-            cantidadAlumnos++;
-
-            // Para poder actualizar la cantidad desde el bucle necesitamos tener una seccion de entrada que referencie a notas dentro del mismo
-            nota = input.nextInt();
+            if (nota!=-1) {
+                // Igualamos la suma a la nota más si misma esto hara que cuando añadamos más notas se nos acumule
+                sumaNotas = nota + sumaNotas;
+                cantidadAlumnos++;
+            }
 
 
         }while (nota!=-1);
-        media = sumaNotas/cantidadAlumnos;
-        System.out.println("La media de notas de los alumnos es " + media);
+
+        System.out.println("La media de notas de los alumnos es " + sumaNotas/cantidadAlumnos);
     }}
