fc61fea5cf19b519c1e71139830dea6cf0eb26fc
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-24 14:13:38 +0200

    Documentando

--- Boletin5/src/Ejercicio1.java
+++ Boletin5/src/Ejercicio1.java
@@ -1,12 +1,15 @@
 
 public class Ejercicio1 {
     public static void main(String[] args) {
+        // Creamos variables para definir la cantidad de los tipos de valores que en principio es 0
         int valoresNegativos = 0;
         int valoresPositivos = 0;
         int valorCero = 0;
-        // Aqui creo una lista que contiene los valores
+
+        // Aqui creo una lista que contiene los valores tanto positivos como negativos
         int[] valores = {20, -56, 43, -3, 0, -10, 67, 10, -6, 100};
 
+        /* Introduzco un bucle for que itere cada elemento de la lista, luego usamos un condicional*/
         for(int cantNum = 0; cantNum<valores.length;cantNum++) {
             if (valores[cantNum] < 0) {
                 valoresNegativos++;
