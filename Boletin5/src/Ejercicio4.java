512002da6aba486ea8ff486160e4d0997e778047
Oscar <oscarodriguez56@gmail.com>
2024-10-26 19:25:36 +0200

    Propuesta para el Ejercicio4

--- Boletin5/src/Ejercicio4.java
+++ Boletin5/src/Ejercicio4.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Ejercicio4 {
     public static void main(String[] args) {
+        int numeroMult = 0,control;
+        // Definimos el Scanner
+        Scanner lectura = new Scanner(System.in);
 
+        //Hacemos la lectura del numero
+        int numero = lectura.nextInt();
+
+        do {
+            int numeroFinal = numero + numeroMult;
+            System.out.println(numero + "x" + numeroMult + " = " + numeroFinal);
+            numeroMult++;
+            control = lectura.nextInt();
+        } while (control!=0);
     }
 }
