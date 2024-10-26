14b8c5a8092bdb2d63d55ace1e5a246a8657bd21
Oscar <oscarodriguez56@gmail.com>
2024-10-26 21:47:24 +0200

    Propuesta para el Ejercicio4

--- Boletin5/src/Ejercicio4.java
+++ Boletin5/src/Ejercicio4.java
@@ -6,14 +6,23 @@ public class Ejercicio4 {
         // Definimos el Scanner
         Scanner lectura = new Scanner(System.in);
 
+        // Indicamos que queremos que se introduzca un numero
+        System.out.print("Introduce el número base para multiplicar (0 para terminar): ");
         //Hacemos la lectura del numero
         int numero = lectura.nextInt();
 
         do {
-            int numeroFinal = numero + numeroMult;
+            int numeroFinal = numero * numeroMult;
             System.out.println(numero + "x" + numeroMult + " = " + numeroFinal);
             numeroMult++;
-            control = lectura.nextInt();
+            if (lectura.hasNextInt()) {
+                control = lectura.nextInt();
+        }
+            else {
+                lectura.next(); // Limpiar la entrada inválida
+                control = -1; // Continuar el bucle
+        }
         } while (control!=0);
+        lectura.close();
     }
 }
