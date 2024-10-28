0d5afc4336ead131b4c41b0753e4a1180e86394e
Oscar <oscarodriguez56@gmail.com>
2024-10-28 19:21:50 +0100

    Acabado el Ejercicio7

--- Boletin5/src/Ejercicio7.java
+++ Boletin5/src/Ejercicio7.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Ejercicio7 {
     public static void main(String[] args) {
-
+        Scanner pedir = new Scanner(System.in);
+        String caract ="";
+        int numero = pedir.nextInt();
+        for (int i=1;i<=numero;i++) {
+            for (int x = numero - i;x>0;x--) {
+                System.out.print(" ");
+            }
+           for (int k = 1; k <= i; k++) {
+                System.out.print("* "); // Imprime un asterisco seguido de un espacio
+            }
+            System.out.println();
+        }
     }
 }
