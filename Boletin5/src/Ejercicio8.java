1f9c70497662b18e169b84718372423e4009e9e0
Oscar <oscarodriguez56@gmail.com>
2024-10-28 14:26:57 +0100

    Añadiendo Ejercicio8

---
+++ Boletin5/src/Ejercicio8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        int numero=0, suma=0;
+        Scanner datos = new Scanner(System.in);
+
+        for (int y=1; y<=10;y++) {
+            System.out.print("Introduzca numero (salir con 999):");
+            numero = datos.nextInt();
+                if (numero==999) {
+                    break;
+                }
+                suma += numero;
+            }
+        System.out.println(suma);
+        datos.close();
+        }
+    }
