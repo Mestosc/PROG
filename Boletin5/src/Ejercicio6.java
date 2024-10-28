d4538b568ca407a07b11841e233b407da5ceab77
Oscar <oscarodriguez56@gmail.com>
2024-10-28 18:36:40 +0100

    Corrigiendo el seis

--- Boletin5/src/Ejercicio6.java
+++ Boletin5/src/Ejercicio6.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Ejercicio6 {
     public static void main(String[] args) {
-
+        Scanner dato = new Scanner(System.in);
+        int numero = dato.nextInt();
+        long factorial=1; //Declaramos la variable como long para si da un numero cuyo factorial sea muy largo
+        for (int i=1;i<numero;i++) {
+            factorial *=i;
+        }
+        System.out.printf("El factorial de %d es: %,d\n", numero, (numero*factorial));
     }
 }
