9f534202a251c87e40ec2975e640a91bab026e16
Oscar <oscarodriguez56@gmail.com>
2024-10-28 13:47:55 +0100

    Ejercicio 5 finalizado

--- Boletin5/src/Ejercicio5.java
+++ Boletin5/src/Ejercicio5.java
@@ -5,25 +5,26 @@ public class Ejercicio5 {
         double sueldo;
         int cantTrabajadoresMas1000=0, cantTrabajadoresMenos1000=0, trabajdoresTotal=0;
         Scanner recibir = new Scanner(System.in);
-        System.out.print("Introduzca sueldo");
-        sueldo = recibir.nextInt();
+        System.out.print("Introduzca sueldo:");
+        sueldo = recibir.nextDouble();
         while (sueldo!=0) {
             if (sueldo>0) {
              if (sueldo>=1000 && sueldo<=1750) {
                 cantTrabajadoresMas1000++;
                 trabajdoresTotal++;
-                sueldo = recibir.nextInt();
+                sueldo = recibir.nextDouble();
             }
              else if (sueldo<1000) {
                 cantTrabajadoresMenos1000++;
                 trabajdoresTotal++;
-                sueldo = recibir.nextInt();
+                sueldo = recibir.nextDouble();
             }
         } else {
-            sueldo = recibir.nextInt();}
+            sueldo = recibir.nextDouble();}
         }
         if (trabajdoresTotal>0) {
-            System.out.println("El porcentaje de trabajadores que cobran entre 1.000 y 1.750 es:" + (cantTrabajadoresMas1000 / trabajdoresTotal) * 100);
+            System.out.printf("El porcentaje de trabajadores que cobran entre 1.000 y 1.750 es: %.2f%%\n", ((double)cantTrabajadoresMas1000 /(double)trabajdoresTotal) * 100);
+            System.out.printf("El porcentaje de trabajadores que cobran entre que cobran menos de 1.000 es: %.2f%%\n", (cantTrabajadoresMenos1000 /(double)trabajdoresTotal) * 100);
         }
         else {
             System.out.println("No se han introducido trabajadores es imposible hacer un calculo");
