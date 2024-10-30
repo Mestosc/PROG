a4dcb21a87edc773a521ec752bf651749a318329
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-30 18:41:24 +0100

    Funcion calcular area y volumen cubo implementada

--- Boletin6/src/Ejercicio3.java
+++ Boletin6/src/Ejercicio3.java
@@ -2,23 +2,28 @@ import java.util.Scanner;
 
 public class Ejercicio3 {
     public static void main(String[] args) {
-        int caso,longitud;
+        int caso, longitud;
         Scanner datos = new Scanner(System.in);
         System.out.print("Indica si deseas calcular area o volumen usando numeros 1(area), 2(volumen");
         caso = datos.nextInt();
         System.out.print("Indicame la longitud del cuadrado");
         longitud = datos.nextInt();
-        calcularAreaCubo(caso,longitud);
+        calcularCubo(caso, longitud);
 
     }
 
     /**
      * Calculara el area o el volumen, segun lo que se indique y necesitaremos pasarle la longitud del lado
-     * @param caso si calculamos area o volumen
+     *
+     * @param caso         si calculamos area o volumen
      * @param longitudLado la longitud del lado del cubo
      */
-    static void calcularAreaCubo(int caso, int longitudLado) {
-
-
+    static void calcularCubo(int caso, int longitudLado) {
+        if (caso==1) {
+            System.out.println("El area del cubo cuyo lado es " + longitudLado + " " + (6*(longitudLado*longitudLado)));
+        }
+        else {
+            System.out.println("El volumen del cubo cuyo lado es " + longitudLado + " " + (longitudLado*longitudLado*longitudLado));
+        }
     }
 }
