7f1be961e800f9f7936915fc59b2b57479d93dc0
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-30 14:21:20 +0100

    Añadiendo documentacion

--- Boletin6/src/Ejercicio3.java
+++ Boletin6/src/Ejercicio3.java
@@ -4,16 +4,21 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         int area,volumen,longitud;
         Scanner datos = new Scanner(System.in);
-        System.out.print("Indicame el area del cubo:");
-        area = datos.nextInt();
-        System.out.print("Indicame el volumen del cubo:");
-        volumen = datos.nextInt();
-        System.out.print("Indicame la longitud del cubo:");
+        System.out.print("Indica si deseas calcular area o volumen usando numeros 1(area), 2(volumen");
+        int caso = datos.nextInt();
+        System.out.print("Indicame la longitud del cuadrado");
         longitud = datos.nextInt();
-        calcularAreaCubo(area,volumen,longitud);
+        calcularAreaCubo(caso,longitud);
 
     }
-    static void calcularAreaCubo(int area, int volume, int longitudLado) {
+
+    /**
+     * Esta funcion calculara el area o el volumen, segun lo que se indique y necesitaremos pasarle la longitud del lado
+     * @param caso si calculamos area o volumen
+     * @param longitudLado la longitud del lado del cubo
+     */
+    static void calcularAreaCubo(int caso, int longitudLado) {
+
 
     }
 }
