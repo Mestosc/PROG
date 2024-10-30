156f908c65c99156b0b1d68e08d74e0951659ca9
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-30 08:49:33 +0100

    Ejercicio7 simplificando comentarios

--- Boletin5/src/Ejercicio7.java
+++ Boletin5/src/Ejercicio7.java
@@ -5,11 +5,14 @@ public class Ejercicio7 {
         Scanner pedir = new Scanner(System.in);
         System.out.println("Introduzca un numero:");
         int numero = pedir.nextInt();
-        for (int i=1;i<=numero;i++) {
-            for (int x = numero - i;x>0;x--) {
+        for (int i=1;i<=numero;i++) { // Este bucle for verifica si la variable de control es menor o igual al numero
+            for (int x = numero - i;x>0;x--) { /* Este bucle imprime espacios en cada iteración, calculando (numero-i) para determinar cuántos espacios se imprimen.
+            La variable de control decrementa en cada ciclo para reducir los espacios impresos progresivamente.*/
                 System.out.print(" ");
             }
-           for (int k = 1; k <= i; k++) {
+           for (int k = 1; k <= i; k++) { /* Este bucle comprueba si esta variable de control es igual o menor a la del bucle principal
+           haciendo una iteracion para imprimir tantos asteriscos como la variable de control del primer bucle
+           */
                 System.out.print("* "); // Imprime un asterisco seguido de un espacio
             }
             System.out.println();
