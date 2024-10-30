be781f753e94915d6f8f9f6ca1f68fe8b7f9f794
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-30 14:22:06 +0100

    Añadiendo documentacion

--- Boletin6/src/Ejercicio3.java
+++ Boletin6/src/Ejercicio3.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Ejercicio3 {
     public static void main(String[] args) {
-        int area,volumen,longitud;
+        int caso,longitud;
         Scanner datos = new Scanner(System.in);
         System.out.print("Indica si deseas calcular area o volumen usando numeros 1(area), 2(volumen");
-        int caso = datos.nextInt();
+        caso = datos.nextInt();
         System.out.print("Indicame la longitud del cuadrado");
         longitud = datos.nextInt();
         calcularAreaCubo(caso,longitud);
