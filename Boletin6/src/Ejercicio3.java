87309d724905fc3ae435873faca5038a0711ca6c
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-30 18:43:51 +0100

    Mejorando legibilidad de los mensajes informativos

--- Boletin6/src/Ejercicio3.java
+++ Boletin6/src/Ejercicio3.java
@@ -4,9 +4,9 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         int caso, longitud;
         Scanner datos = new Scanner(System.in);
-        System.out.print("Indica si deseas calcular area o volumen usando numeros 1(area), 2(volumen");
+        System.out.print("Indica si deseas calcular area o volumen usando numeros 1(area), 2(volumen):");
         caso = datos.nextInt();
-        System.out.print("Indicame la longitud del cuadrado");
+        System.out.print("Indicame la longitud del cuadrado:");
         longitud = datos.nextInt();
         calcularCubo(caso, longitud);
 
@@ -20,10 +20,10 @@ public class Ejercicio3 {
      */
     static void calcularCubo(int caso, int longitudLado) {
         if (caso==1) {
-            System.out.println("El area del cubo cuyo lado es " + longitudLado + " " + (6*(longitudLado*longitudLado)));
+            System.out.println("El area del cubo cuyo lado es " + longitudLado + ": " + (6*(longitudLado*longitudLado)));
         }
         else {
-            System.out.println("El volumen del cubo cuyo lado es " + longitudLado + " " + (longitudLado*longitudLado*longitudLado));
+            System.out.println("El volumen del cubo cuyo lado es " + longitudLado + ": " + (longitudLado*longitudLado*longitudLado));
         }
     }
 }
