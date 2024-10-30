653a147e9021e015be857d3c27bd179ed3e02ff0
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-30 14:14:33 +0100

    Añadiendo documentacion

--- Boletin6/src/Ejercicio2.java
+++ Boletin6/src/Ejercicio2.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
-
+/**
+ * Nos solicita un numero y nos permite imprimir Eco... varias veces con una funcion
+ * **/
 public class Ejercicio2 {
     public static void main(String[] args) {
         System.out.println("Introduzca un numero");
@@ -7,6 +9,11 @@ public class Ejercicio2 {
         eco(introduccion.nextInt());
         introduccion.close();
     }
+
+    /**
+     * Nos muestra la palabra Eco... una cantidad n de veces
+     * @param veces la cantidad de veces que muestra Eco...
+     */
     static void eco(int veces) {
         for (int i = 0;i<veces;i++) {
             System.out.println("Ecoo...");
