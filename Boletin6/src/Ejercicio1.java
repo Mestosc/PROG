fa50d230c9941fd22a96ba7f74b0627e248d73f9
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-10-30 10:47:07 +0100

    Añadiendo Ejercicio1

---
+++ Boletin6/src/Ejercicio1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner introduccionDato = new Scanner(System.in);
+        System.out.println("Introduzca un numero:");
+        int numero1 = introduccionDato.nextInt();
+        System.out.println("Introduzca otro numero:");
+        int numero2 = introduccionDato.nextInt();
+        funcionNumerosEntre(numero1,numero2);
+
+    }
+    static void funcionNumerosEntre(int num1, int num2) {
+        int resultado = 0;
+        for (int i = num1+1; i>=num1 && i<=num2;i++) {
+            System.out.println(i);
+        }
+    }
+}
