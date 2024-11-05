70389ba4eb8cdf13b532300c2146c595271c6497
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-11-05 13:04:14 +0100

    Funcion mostrarFibonnaci acabada

---
+++ Boletin6/src/Ejercicio9.java
@@ -0,0 +1,20 @@
+public class Ejercicio9 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Funcion para devolver los numeros de Fibonnaci
+     * @param num1 el numero del que queremos sacar la serie
+     * @return Los numeros de la serie de Fibonnaci
+     */
+    static int mostrarFibonnaci(int num1) {
+        if (num1==0) {
+            return 0;
+        }
+        else if (num1==1){
+            return 1;
+        }
+        return mostrarFibonnaci(num1-1) + mostrarFibonnaci(num1-2);
+    }
+}
