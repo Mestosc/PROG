07fb7fbd15beb4b3775df137c55e262b244a5398
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-11-10 20:52:40 +0100

    Ejercicio1 añadiendole aleatoriedad requerida

--- Boletin7/src/Ejercicio1.java
+++ Boletin7/src/Ejercicio1.java
@@ -1,6 +1,9 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
-        int[] numeros = {49,23,12,40,36,4}; // Estableciendo lista de numeros
+        int[] numeros = new int[6]; // Estableciendo lista de numeros
+        for (int j = 0;j<numeros.length;j++) {
+            numeros[j] = (int)Math.floor(Math.random()*51); // Añadiendo numeros aleatorios entre el 1 y el 50 ambos incluidos a cada espacio del array
+        }
         for (int i = numeros.length-1;i>0;i--){ /* Hacer un bucle for
         para iterar de forma inversa los numeros
         */
