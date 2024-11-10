e75ae7be02b65fd876936caa3aa822d480e98e76
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-11-10 20:37:35 +0100

    Ejercicio1 finalizado

---
+++ Boletin7/src/Ejercicio1.java
@@ -0,0 +1,10 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int[] numeros = {49,23,12,40,36,4}; // Estableciendo lista de numeros
+        for (int i = numeros.length-1;i>0;i--){ /* Hacer un bucle for
+        para iterar de forma inversa los numeros
+        */
+            System.out.println(numeros[i]); //Mostrar cada uno de los numeros de la lista
+        }
+    }
+}
