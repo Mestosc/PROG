fbb427529657ca9f12f407504e9f24922464283b
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-11-11 08:35:34 +0100

    Especificando mas...

--- Boletin7/src/Ejercicio1.java
+++ Boletin7/src/Ejercicio1.java
@@ -10,7 +10,7 @@ public class Ejercicio1 {
             numeros[j] = (int)Math.floor(Math.random()*51); // Añadiendo numeros aleatorios entre el 1 y el 50 ambos incluidos a cada espacio del array
         }
         for (int i = numeros.length-1;i>0;i--){ /* Hacer un bucle for
-        para iterar de forma inversa los numeros
+        para iterar de forma inversa los numeros del array para obtener las posiciones al reves
         */
             System.out.println(numeros[i]); //Mostrar cada uno de los numeros de la lista
         }
