ac03e8f2e3c7918d711bac5b1772851dde757253
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-11-20 09:02:01 +0100

    Comentando codigo

--- Boletin7/src/Ejercicio4.java
+++ Boletin7/src/Ejercicio4.java
@@ -9,7 +9,7 @@ public class Ejercicio4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char[] letras = {'T' ,'R', 'W', 'A', 'G', 'M', 'Y', 'F' ,'P', 'D', 'X', 'B', 'N', 'J',
-                'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+                'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'}; // Array que contiene las letras letras del DNI
         System.out.print("Introduzca su DNI y yo le dire la letra:");
         var dni = sc.nextInt(); // Añadimos el Scanner para obtener el numero del DNI
         System.out.println("La letra de su DNI es:" + letras[dni%23]); /* En este caso en especifico el indice de la tabla letras es el resto del  numero del dni / 23 */
