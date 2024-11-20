1e03e12e90ae6380fac8d48734e2f67882b423bd
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-11-20 11:04:56 +0100

    Cambiando cosas

--- Boletin7/src/Ejercicio3.java
+++ Boletin7/src/Ejercicio3.java
@@ -59,7 +59,7 @@ public class Ejercicio3 {
             for (int j = 0; j<list.length-1-i; j++) {
                 if (list[j]> list[j+1]) {
                     int temp = list[j];
-                    list[j] = list[j + 1];
+                    list[j] = list[j+1];
                     list[j+1] = temp;
                 }
             }
