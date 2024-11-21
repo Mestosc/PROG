72b3d0cfb93fa6a18ac3b1844eee831617a30c85
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-11-21 16:21:29 +0100

    Cambio menor a la lista

--- EjerciciosVarios/src/Obtenerindice.java
+++ EjerciciosVarios/src/Obtenerindice.java
@@ -3,7 +3,7 @@
  */
 public class Obtenerindice {
     public static void main(String[] args) {
-        int[] prueba = {1,2,3,4,5,6,7,8,9};
+        int[] prueba = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22};
         System.out.println(obtenerIndiceListaOrdenados(prueba,4));
         int[] tablaD;
         tablaD = dividirTabla(prueba);
