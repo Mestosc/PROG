0966fc7175abc5470852511e502c90680091f64b
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-11-21 16:54:43 +0100

    Haciendo correcciones

--- EjerciciosVarios/src/Obtenerindice.java
+++ EjerciciosVarios/src/Obtenerindice.java
@@ -4,7 +4,7 @@
 public class Obtenerindice {
     public static void main(String[] args) {
         int[] prueba = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22};
-        System.out.println(obtenerIndiceListaOrdenados(prueba,4));
+        System.out.println(obtenerIndiceListaOrdenados(prueba,6));
         int[] tablaD;
         tablaD = dividirTabla(prueba);
 
@@ -47,18 +47,26 @@ public class Obtenerindice {
                 }
             }}
     }
+
+    /**
+     * De una lista de numeros enteros ordenados saca el indice del numero deseado
+     * @param lista la lista de enteros de la que queremos saber un indice de numeros
+     * @param num el numero del que queremos saber el indice
+     * @return el indice del numero
+     */
     static int obtenerIndiceListaOrdenados(int[] lista, int num) {
-        int mitad = lista.length / 2;
-        int mitadD = mitad/2;
-        if (lista[mitad]<num) {
-            if (lista[mitadD]<num) {
-                for (int i = mitadD+1;i<lista.length;i++) {
-                    if (lista[i]==num) {
-                        return i;
+        int mitad = lista.length / 2; // La mitad de la lista
+        int mitadD = mitad+(mitad/2); // La mitad de la derecha
+        int mitadI = mitad/2;
+        if (lista[mitad]<num) { // Si el numero es mayor a lo que hay en la mitad de la lista
+            if (lista[mitadD]<num) { // Verificaremos primero si es mayor a lo que hay en la segunda mitad de la mitad derecha de la lista
+                for (int i = mitadD+1;i<lista.length;i++) { // Si asi es procederemos a iterar desde un numero mas que la segunda mitad hasta el final de la lista
+                    if (lista[i]==num) { // Comprobaremos si los elementos de esa iteracion son iguales a num en cuyo caso devolveremos el indice y acabariamos ahi la funcion
+                        return i; // Devolviendo el indice
                     }
                 }
             }
-            else if (lista[mitadD]==num) {
+            else if (lista[mitadD]==num) { // Si el numero es igual a la segunda mitad devolvemos eso
                 return mitadD;
             }
             else {
@@ -73,16 +81,16 @@ public class Obtenerindice {
             return mitad;
         }
         else {
-            if (num>mitadD) {
-                for (int i = mitadD+1;i<mitad;i++) {
+            if (num>lista[mitadI]) {
+                for (int i = mitadI+1;i<mitad;i++) {
                     if (lista[i]==num) {
                         return i;
                     }
                 }
             }
-            else if (num==mitadD) return mitadD;
+            else if (num==lista[mitadI]) return mitadI;
             else {
-                for (int i = 0;i<mitadD;i++) {
+                for (int i = 0;i<mitadI;i++) {
                     if (lista[i]==num) {
                         return i;
                     }
