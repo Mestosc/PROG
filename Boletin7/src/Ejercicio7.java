e951da4208d14c68ad1378b838fe5cd2f2fda587
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-12-05 13:09:59 +0100

    Añadiendo cambios

--- Boletin7/src/Ejercicio7.java
+++ Boletin7/src/Ejercicio7.java
@@ -1,6 +1,11 @@
 public class Ejercicio7 {
     public static void main(String[] args) {
         int[] matriz = {12,134,145,164,67,54,13,778,54,32};
+        mostrarOriginalNueva(matriz); // Muestra la matriz nueva y la original
+
+    }
+
+    static void mostrarOriginalNueva(int[] matriz) {
         System.out.println("Comienza la matriz original");
         for (int matriz1 : matriz) {
             System.out.println(matriz1);
@@ -10,8 +15,8 @@ public class Ejercicio7 {
             System.out.println(matriz1);
         }
         System.out.println("Finaliza la matriz copia");
-
     }
+
     static int[] CopiaMatriz(int[] matriz) {
         int[] nuevaMatriz = new int [matriz.length];
         for (int i = 0; i<nuevaMatriz.length;i++) {
