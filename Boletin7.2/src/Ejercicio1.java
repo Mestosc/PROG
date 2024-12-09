42a306c8a78faa472489871a0e13c1948a28c6c8
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-12-09 10:50:21 +0100

    Dejandolo como lo tenia

--- Boletin7.2/src/Ejercicio1.java
+++ Boletin7.2/src/Ejercicio1.java
@@ -24,10 +24,11 @@ public class Ejercicio1 {
         } while (opcion==3);
     }
     static void mostrarHorario(String[][] horario) {
-        for (String[] horar : horario) {
-            for (String hora : horar) {
-                System.out.println(hora);
+        for (int i = 0; i < horario.length; i++) {
+            for (int j = 0; j < horario[i].length; j++) {
+                System.out.print(horario[i][j]);
             }
+            System.out.println();
         }
     }
     static void cambiarActividad(String[][] horario,Scanner sc) {
