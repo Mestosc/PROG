ca88ed3d9f1eaa253dbd77ec34348a87e8b5bc3c
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-12-18 10:17:13 +0100

    Funcion mostrar palabra caracter por caracter acabada

---
+++ Boletin8/src/Ejercicio2.java
@@ -0,0 +1,11 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        String esto = "Java";
+
+    }
+    void mostrarStringPartes(String prueba) {
+        for (int i = 0; i < prueba.length(); i++) {
+            System.out.println(prueba.charAt(i));
+        }
+    }
+}
