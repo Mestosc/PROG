f008a77445ad4a54de9aaa8e5e839bc3618ce58a
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-12-18 11:27:39 +0100

    Funcion mostrarCuantasConsonantesVocales finalizada

---
+++ Boletin8/src/Ejercicio5.java
@@ -0,0 +1,29 @@
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        String texto = "java java java";
+        mostrarCuantasConsonantesVocales(texto);
+    }
+
+    /**
+     * Muestra la cantidad de vocales y consonantes hay en un texto
+     * @param text el texto que queremos verificar
+     */
+    static void mostrarCuantasConsonantesVocales(String text) {
+        int contadorVocales=0,contadorConsonantes=0;
+        text = text.toLowerCase();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            boolean esVocal = c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+            if (esVocal) {
+                contadorVocales++;
+                continue;
+            }
+            else if (c==' ') {
+                continue;
+            }
+            contadorConsonantes++;
+        }
+        System.out.println("Hay " + contadorConsonantes + " consonantes");
+        System.out.println("Hay " + contadorVocales + " vocales");
+    }
+}
