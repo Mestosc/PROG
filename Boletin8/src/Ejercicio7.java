70d57b847c6a27ddb9229268e38d32b8d969e9d9
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-12-18 12:44:12 +0100

    Ejercicio7 finalizado

---
+++ Boletin8/src/Ejercicio7.java
@@ -0,0 +1,8 @@
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        String texto = " javeros";
+        String textoMayuscula = texto.toUpperCase();
+        String textoMinuscula = textoMayuscula.toLowerCase();
+        System.out.println("Mostrar:" + textoMinuscula) ;
+    }
+}
