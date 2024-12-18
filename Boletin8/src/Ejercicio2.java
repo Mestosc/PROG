045ded3c67a2b2d4276a43d9644c98e78f11226d
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-12-18 10:18:18 +0100

    Ejercicio2 acabado

--- Boletin8/src/Ejercicio2.java
+++ Boletin8/src/Ejercicio2.java
@@ -1,9 +1,9 @@
 public class Ejercicio2 {
     public static void main(String[] args) {
         String esto = "Java";
-
+        mostrarStringPartes(esto);
     }
-    void mostrarStringPartes(String prueba) {
+    static void mostrarStringPartes(String prueba) {
         for (int i = 0; i < prueba.length(); i++) {
             System.out.println(prueba.charAt(i));
         }
