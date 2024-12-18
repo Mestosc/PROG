963c1d5ca02b55e6c5ea8e7b493480fab8369245
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-12-18 10:45:11 +0100

    Cambiando codigo por motivos practicos

--- Boletin8/src/Ejercicio4.java
+++ Boletin8/src/Ejercicio4.java
@@ -1,7 +1,7 @@
 public class Ejercicio4 {
     public static void main(String[] args) {
         String hol = "James Gosling Created Java";
-        String sinEspacio = hol.replaceAll(" ","");
-        System.out.println(sinEspacio); // Llamando al metodo replaceAll que te permite pasarle un caracter y cambiar todas las coincidencias por otra cosa
+        String sinEspacio = hol.replace(" ","");
+        System.out.println(sinEspacio); // Llamando al metodo replace que te permite pasarle un caracter y cambiar todas las coincidencias por otra cosa
     }
 }
