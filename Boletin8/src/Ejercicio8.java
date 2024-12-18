909ba0a42ee98b0aa14e060dbec9f0885331a22c
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-12-18 12:46:46 +0100

    Ejercicio8 finalizado

---
+++ Boletin8/src/Ejercicio8.java
@@ -0,0 +1,8 @@
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        String lenguaje1 = "Java";
+        String lenguaje2 = "JavaScript";
+        boolean esIgual = lenguaje1.equalsIgnoreCase(lenguaje2);
+        System.out.println(esIgual);
+    }
+}
