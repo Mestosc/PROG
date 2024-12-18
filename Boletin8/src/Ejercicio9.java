b65f5862c603109d426c623fb5753e57c5faf631
Oscar <orodriguezcabaleiro@danielcastelao.org>
2024-12-18 12:56:12 +0100

    Ejercicio9 finalizado

---
+++ Boletin8/src/Ejercicio9.java
@@ -0,0 +1,7 @@
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        String texto = " Jeve jeve jeve";
+        String textoRep = texto.replace("e","a").toLowerCase();
+        System.out.println(textoRep);
+    }
+}
