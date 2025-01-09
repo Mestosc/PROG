2002289c3a1c1bae15c244f9e568570d7d722bcd
Oscar <orodriguezcabaleiro@danielcastelao.org>
2025-01-09 13:15:11 +0100

    Separacion de caracteres de una cadena a partir de un caracter

--- Boletin_8.2/src/Ejercicio2.java
+++ Boletin_8.2/src/Ejercicio2.java
@@ -1,5 +1,18 @@
 public class Ejercicio2 {
     public static void main(String[] args) {
-
+        String cadena = "separar";
+        System.out.println(anadirCaracter(cadena,','));
+    }
+    static String anadirCaracter(String cadena, char caracter) {
+        StringBuilder cadena_nueva = new StringBuilder();
+        for (int i = 0; i < cadena.length(); i++) {
+            char c = cadena.charAt(i);
+            cadena_nueva.append(c);
+            if (i == cadena.length()-1)  {
+                break;
+            }
+            cadena_nueva.append(caracter);
+        }
+        return cadena_nueva.toString();
     }
 }
