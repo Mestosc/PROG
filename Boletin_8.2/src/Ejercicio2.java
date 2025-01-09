90e5e64515fc973eb061ab0a92176e3d9096e592
Oscar <orodriguezcabaleiro@danielcastelao.org>
2025-01-09 13:27:10 +0100

    Cambios varios

--- Boletin_8.2/src/Ejercicio2.java
+++ Boletin_8.2/src/Ejercicio2.java
@@ -1,11 +1,11 @@
 public class Ejercicio2 {
     public static void main(String[] args) {
         String cadena = "separar";
-        System.out.println(anadirCaracterEntreCaracteres(cadena,','));
+        System.out.println(anadirCaracterEntreCaracteres(cadena));
         String cadena2 = "meu arquivo de texto.txt";
         System.out.println(cadena2.replace(" ","\\_"));
     }
-    static String anadirCaracterEntreCaracteres(String cadena, char caracter) {
+    static String anadirCaracterEntreCaracteres(String cadena) {
         StringBuilder cadena_nueva = new StringBuilder();
         for (int i = 0; i < cadena.length(); i++) {
             char c = cadena.charAt(i);
@@ -13,7 +13,7 @@ public class Ejercicio2 {
             if (i == cadena.length()-1)  {
                 break;
             }
-            cadena_nueva.append(caracter);
+            cadena_nueva.append(',');
         }
         return cadena_nueva.toString(); /* Tambien se podria hacer con String concatenandolo yo al final lo hice asi
         el IDE no se me queja y funciona bien ya que simplemente va añadiendo al final el caracter y luego la coma yo lo hice asi por que de la otra manera me lie luego lo hice asi en el main y asi se quedo*/
