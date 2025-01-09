a9ec38d39793111ce581566a4127b70b9929d54c
Oscar <orodriguezcabaleiro@danielcastelao.org>
2025-01-09 13:26:19 +0100

    Cambiando nombre a funcion para hacerlo más claro

--- Boletin_8.2/src/Ejercicio2.java
+++ Boletin_8.2/src/Ejercicio2.java
@@ -1,11 +1,11 @@
 public class Ejercicio2 {
     public static void main(String[] args) {
         String cadena = "separar";
-        System.out.println(anadirCaracter(cadena,','));
+        System.out.println(anadirCaracterEntreCaracteres(cadena,','));
         String cadena2 = "meu arquivo de texto.txt";
         System.out.println(cadena2.replace(" ","\\_"));
     }
-    static String anadirCaracter(String cadena, char caracter) {
+    static String anadirCaracterEntreCaracteres(String cadena, char caracter) {
         StringBuilder cadena_nueva = new StringBuilder();
         for (int i = 0; i < cadena.length(); i++) {
             char c = cadena.charAt(i);
