3a93884c7403f6fee87be2dc5d562e6f8a61cc1f
Oscar <orodriguezcabaleiro@danielcastelao.org>
2025-01-09 21:28:19 +0100

    Funcion cadenaSoloVocales y cadenaConSiguienteVocal finalizadas

--- Boletin_8.2/src/Ejercicio6.java
+++ Boletin_8.2/src/Ejercicio6.java
@@ -2,6 +2,36 @@ public class Ejercicio6 {
     public static void main(String[] args) {
         System.out.println(saberPalindromo("anita lava la tina"));
         System.out.println(cadenaSoloConsonantes("logaritmos"));
+        System.out.println(cadenaSoloVocales("consonantes"));
+        System.out.println(cadenaConSiguienteVocal("vestiario"));
+    }
+    static String cadenaConSiguienteVocal(String texto) {
+        StringBuilder nuevaCadena = new StringBuilder();
+        for (int i = 0; i < texto.length(); i++) {
+            char c = texto.charAt(i);
+            c = switch (c) {
+                case 'a'-> 'e';
+                case 'e'-> 'i';
+                case 'i'-> 'o';
+                case 'o'->'u';
+                case 'u'->'a';
+                default -> c;
+            }; // Uso del switch case 'nuevo' para hacer que si el caracter coincide con una de las vocales existentes se cambie con la siguiente
+            nuevaCadena.append(c);
+        }
+        return nuevaCadena.toString();
+    }
+    static String cadenaSoloVocales(String texto) {
+        texto = texto.toLowerCase();
+        StringBuilder cadena = new StringBuilder();
+        for (int i = 0; i < texto.length() ; i++) {
+            char c = texto.charAt(i);
+            boolean esVocal = c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+            if (esVocal) {
+                cadena.append(c);
+            }
+        }
+        return cadena.toString();
     }
     static String cadenaSoloConsonantes(String texto) {
         texto = texto.toLowerCase();
