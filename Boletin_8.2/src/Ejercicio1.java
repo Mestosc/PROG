52829abee7b47691151a06ad9e4df7ab5eec0a44
Oscar <orodriguezcabaleiro@danielcastelao.org>
2025-01-09 13:24:30 +0100

    Cambiando funcion imprimirTresUltimos caracteres

--- Boletin_8.2/src/Ejercicio1.java
+++ Boletin_8.2/src/Ejercicio1.java
@@ -10,8 +10,7 @@ public class Ejercicio1 {
         System.out.println(texto.substring(0,2));
     }
     static void imprimirTresUltimosCaracteres(String texto) {
-        int longitud = texto.length();
-        System.out.println(texto.substring(longitud-3,longitud));
+        System.out.println(texto.substring( texto.length()-3));
     }
     static void imprimirCadaDos(String texto) {
         for (int i = 0;i<texto.length();i+=2) {
