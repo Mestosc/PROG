edfbfaf5a4b91f8d8282f54f5a7c087e57546a1d
Oscar <orodriguezcabaleiro@danielcastelao.org>
2025-01-09 12:39:07 +0100

    Funcion imprimirPrimerosCaracteres que imprima los dos primeros caracteres implementada

---
+++ Boletin_8.2/src/Ejercicio1.java
@@ -0,0 +1,8 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+    }
+    static void imprimirPrimerosCaracteres(String texto) {
+        System.out.println(texto.substring(0,2));
+    }
+}
