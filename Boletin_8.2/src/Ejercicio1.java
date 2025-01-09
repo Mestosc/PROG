e36bcc1190360e72faf4f5974681ae70f93f0cf5
Oscar <orodriguezcabaleiro@danielcastelao.org>
2025-01-09 12:45:13 +0100

    Funcion imprimir Tres ultimos caracteres

--- Boletin_8.2/src/Ejercicio1.java
+++ Boletin_8.2/src/Ejercicio1.java
@@ -1,8 +1,11 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
-
     }
     static void imprimirPrimerosCaracteres(String texto) {
         System.out.println(texto.substring(0,2));
+    }
+    static void imprimirTresUltimosCaracteres(String texto) {
+        int longitud = texto.length();
+        System.out.println(texto.substring(longitud-3,longitud));
     }
 }
