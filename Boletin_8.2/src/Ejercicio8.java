74882933e6da513836c23bb4976df40d3785f3c9
Oscar <orodriguezcabaleiro@danielcastelao.org>
2025-01-10 15:39:43 +0100

    Metodo de conversion manual

--- Boletin_8.2/src/Ejercicio8.java
+++ Boletin_8.2/src/Ejercicio8.java
@@ -1,5 +1,14 @@
 public class Ejercicio8 {
     public static void main(String[] args) {
         System.out.println(Integer.parseInt("011010",2));
+        System.out.println(conversionManual("011010"));
+    }
+    static int conversionManual(String numero) {
+        int numero_entero = 0;
+        for (int i =0; i < numero.length(); i++) {
+            int bit = Character.getNumericValue(numero.charAt(numero.length()-1-i));
+            numero_entero += (int) (bit * Math.pow(2,i));
+        }
+        return numero_entero;
     }
 }
