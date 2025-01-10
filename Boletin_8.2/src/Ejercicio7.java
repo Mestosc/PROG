fe7feedaec77384ca690dd323af906411cb1ba0b
Oscar <orodriguezcabaleiro@danielcastelao.org>
2025-01-10 15:22:38 +0100

    Metodo saber si un texto es subcadena acabado

--- Boletin_8.2/src/Ejercicio7.java
+++ Boletin_8.2/src/Ejercicio7.java
@@ -2,16 +2,10 @@
 public class Ejercicio7 {
     public static void main(String[] args) {
         System.out.println(entreDosAnteriorOrdenAlfabetico("gnome","kde"));
+        System.out.println(obtenerSiSubcadena("subcadea","cadea"));
     }
     static boolean obtenerSiSubcadena(String cadena1,String cadena2) {
-        boolean esSubcadena = false;
-        for (int i = 0, j = 2; i < cadena1.length() && j < cadena2.length(); i++,j++) {
-            char cadena1prob = cadena1.charAt(i);
-            char cadena2prob = cadena2.charAt(j);
-            if (cadena1prob==cadena2prob) {
-
-            }
-        }
+        return cadena1.contains(cadena2);
     }
     static String entreDosAnteriorOrdenAlfabetico(String texto1,String texto2) {
        char texto1Primera = texto1.charAt(0);
