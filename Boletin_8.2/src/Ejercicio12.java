ac24511036e3c35a240d720e8a1a2bfe55b2014e
Oscar <orodriguezcabaleiro@danielcastelao.org>
2025-01-16 21:48:28 +0100

    Ejercicio12 en proceso

---
+++ Boletin_8.2/src/Ejercicio12.java
@@ -0,0 +1,37 @@
+public class Ejercicio12 {
+    public static void main(String[] args) {
+        String texto = "Hola como estas bello amigo, tu presencia me es grata";
+        String[] palabras = palabrasDivididas(texto);
+        System.out.println(contarPalabras(palabras));
+        System.out.println(palabraMasLarga(palabras));
+    }
+    static String[] palabrasDivididas(String texto) {
+       return texto.split(" ");
+    }
+    static int contarPalabras(String[] palabras) {
+        int contador = 0;
+        return contador;
+    }
+    static void eliminarSignosRaros(String[] palabras) {
+        for (int i = 0;i<palabras.length;i++) {
+            String nueva = "";
+            for (int j = 0; j < palabras[i].length(); j++) {
+                char c = palabras[i].charAt(j);
+                if (Character.isLetter(c)) {
+                    nueva += c;
+                }
+            }
+            palabras[i] = nueva;
+        }
+    }
+    static String palabraMasLarga(String[] palabra) {
+        eliminarSignosRaros(palabra);
+        int masLarga = 0;
+        for (int i = 0; i < palabra.length; i++) {
+            if (palabra[masLarga].length()<palabra[i].length()) {
+                masLarga = i;
+            }
+        }
+        return palabra[masLarga];
+    }
+}
