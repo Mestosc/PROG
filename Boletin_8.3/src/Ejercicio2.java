256d4b5a9e93d0bf8d2ec9234a775c37f6669763
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-01-27 14:00:27 +0100

    Añadiendo texto personalizado para pruebas

--- Boletin_8.3/src/Ejercicio2.java
+++ Boletin_8.3/src/Ejercicio2.java
@@ -1,7 +1,11 @@
 public class Ejercicio2 {
     public static void main(String[] args) {
         System.out.println("Introduzca el titulo de una web:");
-        formatear("Academia Web\n Bienvenidos a Academia Web donde aprendemos de todo menos web");
+        String texto = """
+                Ejercicios Programacion
+                Aqui estoy incluyendo los ejercicios más pequeños que nos manda hacer el profesor tipo boletines u otros ejercicios cosas más grandes que se puedan necesitar en repositorios diferentes seran añadidas en esos repositorios cuando sea necesario, si son ejercicios más grande se hara en un proyecto en especifico
+                """;
+        formatear(texto);
     }
     static void formatear(String textoWeb) {
        int indiceSalto = textoWeb.indexOf("\n");
