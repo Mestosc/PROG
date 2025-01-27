c94b064100a445170606cc39f0b871fa5ad26ba8
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-01-27 14:12:02 +0100

    Eliminando irrelevancias del 1

--- Boletin_8.3/src/Ejercicio1.java
+++ Boletin_8.3/src/Ejercicio1.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Ejercicio1 {
     public static void main(String[] args) {
         String palabra = "frigorífico";
