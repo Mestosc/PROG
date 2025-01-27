7a6f3e1f401e102bc78b637e9e1c723f64c96377
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-01-27 14:11:21 +0100

    Pruebas diversas

--- Boletin_8.3/src/Ejercicio2.java
+++ Boletin_8.3/src/Ejercicio2.java
@@ -2,8 +2,10 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         System.out.println("Introduzca el titulo de una web:");
         String texto = """
-                Ejercicios Programacion
-                Aqui estoy incluyendo los ejercicios más pequeños que nos manda hacer el profesor tipo boletines u otros ejercicios cosas más grandes que se puedan necesitar en repositorios diferentes seran añadidas en esos repositorios cuando sea necesario, si son ejercicios más grande se hara en un proyecto en especifico
+                ¿Por qué lo usamos?
+                
+                Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo "Contenido aquí, contenido aquí". Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de "Lorem Ipsum" va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras veces a propósito (por ejemplo insertándole humor y cosas por el estilo).
+                
                 """;
         formatear(texto);
     }
@@ -12,7 +14,7 @@ public class Ejercicio2 {
        String textoAntesSalto = textoWeb.substring(0,indiceSalto);
        String textoDespuesSalto = textoWeb.substring(indiceSalto+1);
        textoAntesSalto = "<h1>" + textoAntesSalto + "</h1>";
-       textoDespuesSalto = "<p>" + textoDespuesSalto.trim() + "</p>";
+       textoDespuesSalto = "<p>\n" + textoDespuesSalto.trim() + "\n   </p>";
        String textoFinal = "<html>" + "\n   " + textoAntesSalto + "\n   " + textoDespuesSalto + "\n" + "</html>";
        System.out.println(textoFinal);
 
