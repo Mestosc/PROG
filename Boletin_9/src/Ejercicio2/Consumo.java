7862420ea0cccb8174b5807708eca5e10e0e6a30
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-17 18:32:16 +0100

    Consumo Euros mejorado

--- Boletin_9/src/Ejercicio2/Consumo.java
+++ Boletin_9/src/Ejercicio2/Consumo.java
@@ -15,7 +15,7 @@ public class Consumo {
         this.prezoGasolina = prezoGasolina;
     }
     public double consumoEuros() {
-        return litros/getTiempo();
+        return (litros/getTiempo())*prezoGasolina;
     }
     public double getTiempo() {
         return km/velocidadMedia;
