3f7623d2b4b38033d79da943785faa7573eb95ae
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-17 14:46:49 +0100

    Haciendo cosas más coherentes

--- Boletin_9/src/Ejercicio1/Main.java
+++ Boletin_9/src/Ejercicio1/Main.java
@@ -4,7 +4,7 @@ package Ejercicio1;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Libro libro1 = new Libro("Dioses malignos","H.P LoveCraft",1990,(short) 10,2);
+        Libro libro1 = new Libro("Dioses malignos","H.P LofCraft",1990,(short) 200,2);
         libro1.amosar();
     }
 }
