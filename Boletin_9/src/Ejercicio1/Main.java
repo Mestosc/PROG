6b52d640eb018373f76fa7403912812307036f58
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-17 14:50:44 +0100

    Haciendo cosas más coherentes

--- Boletin_9/src/Ejercicio1/Main.java
+++ Boletin_9/src/Ejercicio1/Main.java
@@ -1,7 +1,7 @@
 package Ejercicio1;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/// To **Run** code, press <shortcut actionId="Run"/> or
+/// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Libro libro1 = new Libro("Dioses malignos","H.P LofCraft",1990,(short) 200,2);
