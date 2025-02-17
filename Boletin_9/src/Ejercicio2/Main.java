846bce36411e8fdffe1c59b8f04be4658efc3aa2
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-17 18:38:06 +0100

    Cambiando consumoEuros

--- Boletin_9/src/Ejercicio2/Main.java
+++ Boletin_9/src/Ejercicio2/Main.java
@@ -7,5 +7,7 @@ public class Main {
         consumo.setPrezoGasolina(1.57);
         Consumo consumo1 = new Consumo(100,30,30,0.90);
         System.out.println(consumo1.consumoMedio());
+        consumo.setLitros(40);
+        System.out.println(consumo1.velocidadMedia);
     }
 }
