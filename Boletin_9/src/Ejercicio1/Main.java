d5a0e85f954eed453256accb6a1137ee5298f69b
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-19 08:53:00 +0100

    Cambios diversos

--- Boletin_9/src/Ejercicio1/Main.java
+++ Boletin_9/src/Ejercicio1/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Libro libro1 = new Libro("Dioses malignos","H.P LofCraft",1990,(short) 200,6);
         libro1.amosar();
-        Libro libro2 = new Libro("Dioses antiguos","H.P LofCraft",1993,(short) 300,2);
+        Libro libro2 = new Libro("Dioses antiguos","H.P LofCraft",1993,(short) 100,2);
         libro2.amosar();
         Libro libro3 = new Libro("Dioses antiguos y su mundo","H.P LofCraft",1993,(short) 300,5);
         libro3.amosar();
