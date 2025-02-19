2e46e7a285ec988fdccb319e4686c0cb8058804b
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-19 09:06:53 +0100

    Cambios

--- Boletin_9/src/Ejercicio2/Main.java
+++ Boletin_9/src/Ejercicio2/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println(consumo1.consumoMedio());
         consumo.setLitros(40);
         System.out.println(consumo1.getVelocidadMedia());
+        System.out.println(consumo.consumoEuros());
     }
 }
