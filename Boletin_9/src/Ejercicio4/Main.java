5df464a67824d96e2bb19a3bc491fe66d9326f2a
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-26 11:03:59 +0100

    Haciendo metodos de prueba

--- Boletin_9/src/Ejercicio4/Main.java
+++ Boletin_9/src/Ejercicio4/Main.java
@@ -2,6 +2,12 @@ package Ejercicio4;
 
 public class Main {
     public static void main(String[] args) {
-
+        Conta conta1 = new Conta("OJD",20.3,10,"0393203S");
+        Conta conta = new Conta("OJD",256.10,10,"0393203S");
+        conta.ingreso(20);
+        System.out.println("Conta 1 antes:" +conta1.getSaldo());
+        conta.transferencia(conta1,10.10);
+        System.out.println(conta.getSaldo());
+        System.out.println("Conta 1 despues" +conta1.getSaldo());
     }
 }
