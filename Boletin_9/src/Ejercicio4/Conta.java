d132af3901f8b3d9dabd9ce47ea3a478a47e799a
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-26 10:54:33 +0100

    Definiendo incialmente la clase Conta

---
+++ Boletin_9/src/Ejercicio4/Conta.java
@@ -0,0 +1,48 @@
+package Ejercicio4;
+
+public class Conta {
+    private String nome;
+    private String numeroConta;
+    private double tipoInterese;
+    private double saldo;
+    public Conta() {}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        if (numeroConta.length()==8) this.numeroConta = numeroConta;
+    }
+
+    public double getTipoInterese() {
+        return tipoInterese;
+    }
+
+    public void setTipoInterese(double tipoInterese) {
+        this.tipoInterese = tipoInterese;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Conta(String nome, double saldo, double tipoInterese, String numeroConta) {
+        this.nome = nome;
+        this.saldo = saldo;
+        this.tipoInterese = tipoInterese;
+        setNumeroConta(numeroConta);
+    }
+}
