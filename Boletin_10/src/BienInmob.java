1993b01f997675829412a7fec59bd8bba537fddb
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-02-27 15:56:27 +0100

    Añadiendo seters y getters

--- Boletin_10/src/BienInmob.java
+++ Boletin_10/src/BienInmob.java
@@ -3,11 +3,51 @@ public abstract class BienInmob {
     int numero;
     String localidade;
     double prezo;
-    Operacion operacion;
+    private Operacion operacion;
 
-    public BienInmob(String rua, Operacion operacion, double prezo, String localidade, int numero) {
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
         this.rua = rua;
-        this.operacion = operacion;
+    }
+
+    public double getPrezo() {
+        return prezo;
+    }
+
+    public void setPrezo(double prezo) {
+        this.prezo = prezo;
+    }
+
+    public Operacion getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(String operacion) {
+        this.operacion = Operacion.valueOf(operacion.toUpperCase());
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public BienInmob(String rua, String operacion, double prezo, String localidade, int numero) {
+        this.rua = rua;
+        setOperacion(operacion);
         this.prezo = prezo;
         this.localidade = localidade;
         this.numero = numero;
