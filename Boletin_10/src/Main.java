8f5d054e81f9e9305f3902d0fc0480c5301eac1a
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-03-06 14:51:05 +0100

    Añadiendo cosas

--- Boletin_10/src/Main.java
+++ Boletin_10/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        PlazaGaraje prazaGarciaB = new PlazaGaraje("Garcia Barbon","Venta" ,100,"Vigo",12);
+        Vivienda vivienda = new Vivienda("Garcia Barbon","Venta",1200,"Vigo",12,"4C","3B",prazaGarciaB);
+        System.out.println(vivienda);
     }
 }
