0306d70daf10c7d476dc8a1961aa990485e42f14
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-03-12 08:56:05 +0100

    Añadiendo a través de fecha

--- Boletin_10/src/Ejercicio2/Socio.java
+++ Boletin_10/src/Ejercicio2/Socio.java
@@ -5,13 +5,14 @@ public class Socio extends Persoa {
     String direccion;
     String dataAlta;
     double cuotaMensual;
-    String dataIngreso;
+    Fecha dataIngreso;
 
     public Socio(String dni, String nome, String dataIngreso, String dataAlta, String direccion) {
         super(dni, nome);
         this.direccion = direccion;
         this.dataAlta = dataAlta;
-        this.dataIngreso = dataIngreso;
+        String[] dataI = dataIngreso.split("/");
+        this.dataIngreso = new Fecha(Integer.parseInt(dataI[0]),Integer.parseInt(dataI[1]),Integer.parseInt(dataI[2]));
     }
 
     @Override
