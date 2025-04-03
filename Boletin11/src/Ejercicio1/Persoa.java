e29f0694156a0ab12e157a13744e00a747f4729e
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-04-03 12:39:10 +0200

    Definiendo la estructura basica de la clase Persona y declarando algunos metodos basicos

---
+++ Boletin11/src/Ejercicio1/Persoa.java
@@ -0,0 +1,31 @@
+package Ejercicio1;
+
+public class Persoa {
+    private String nome;
+    private String direccion;
+    private String dni;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+}
