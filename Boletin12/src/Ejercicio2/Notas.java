3a9019b8e573ecd386070caef971588d2c3ebc5d
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-04-13 20:40:43 +0200

    Empezando a diseñar logica interna del ejercicio2

---
+++ Boletin12/src/Ejercicio2/Notas.java
@@ -0,0 +1,53 @@
+package Ejercicio2;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Notas {
+    ArrayList<String> notas = new ArrayList<>();
+    public Notas() {
+        notas = leerNotas();
+    }
+    public void escribirNotas() {
+        BufferedWriter escritor = null;
+        try {
+            escritor = new BufferedWriter(new FileWriter("notas.txt"));
+            for (String nota : notas) {
+                escritor.write(nota);
+            }
+        } catch (IOException e) {
+            System.err.println("Se ha dado un error de entrada salida: " + e.getMessage());
+        } finally {
+            if (escritor!=null) {
+                try {
+                    escritor.close();
+                } catch (IOException e) {
+                    System.out.println("Error en cierre" + e.getMessage());
+                }
+            }
+        }
+    }
+    public ArrayList<String> leerNotas() {
+        ArrayList<String> paraNotas = new ArrayList<>();
+        BufferedReader lector = null;
+        try {
+            lector = new BufferedReader(new FileReader("notas.txt"));
+            String lectura = lector.readLine();
+            while (lectura!=null && lectura.isEmpty()) {
+                paraNotas.add(lectura);
+                lectura = lector.readLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Se ha dado un error de entrada salida: " + e.getMessage());
+        } finally {
+            if (lector!=null) {
+                try {
+                    lector.close();
+                } catch (IOException e) {
+                    System.out.println("Error en cierre" + e.getMessage());
+                }
+            }
+        }
+        return paraNotas;
+    }
+}
