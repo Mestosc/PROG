54c818587e4bc4749852645ed4d3e9595fcba7b2
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-04-15 10:32:07 +0200

    Haciendo retoques en la clase Notas

--- Boletin12/src/Ejercicio2/Notas.java
+++ Boletin12/src/Ejercicio2/Notas.java
@@ -6,8 +6,7 @@ import java.util.ArrayList;
 public class Notas {
     ArrayList<String> notas;
     public Notas() {
-        if (notas!=null) notas = leerNotas();
-        else notas = new ArrayList<>();
+        notas = leerNotas();
     }
     public void escribirNota(String nota) {
         notas.add(nota);
@@ -16,9 +15,10 @@ public class Notas {
     private void escribirNota() {
         BufferedWriter escritor = null;
         try {
-            escritor = new BufferedWriter(new FileWriter("notas.txt"));
+            escritor = new BufferedWriter(new FileWriter("notas.txt",true));
             for (String nota : notas) {
                 escritor.write(nota);
+                escritor.newLine();
             }
         } catch (IOException e) {
             System.err.println("Se ha dado un error de entrada salida: " + e.getMessage());
@@ -38,7 +38,7 @@ public class Notas {
         try {
             lector = new BufferedReader(new FileReader("notas.txt"));
             String lectura = lector.readLine();
-            while (lectura!=null && lectura.isEmpty()) {
+            while (lectura!=null && !lectura.isEmpty()) {
                 paraNotas.add(lectura);
                 lectura = lector.readLine();
             }
