751c291fd0da7a643200cb48b6da58bcf6dfccc7
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-05-08 09:04:58 +0200

    Añadiendo nueva informacion

---
+++ Boletin13/src/EjercicioComprobacionExistencias/Main.java
@@ -0,0 +1,11 @@
+package EjercicioComprobacionExistencias;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String,Integer> mapRef = Fichero.obtenerFichero("mapaExistencias.dat");
+        Scanner sc = new Scanner(System.in);
+    }
+}
