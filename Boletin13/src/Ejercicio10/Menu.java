fbda1eaca440aec02ba58fd13980595e8d4b3656
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-05-08 10:52:38 +0200

    Cambiando el condicional ternario del menu por Math.max que es más legible

--- Boletin13/src/Ejercicio10/Menu.java
+++ Boletin13/src/Ejercicio10/Menu.java
@@ -25,7 +25,7 @@ public class Menu {
         System.out.println("Digame cuantos obtuvo por cierto la perdida debe expresarla precedida con un -:");
         var new_value = sc.nextInt();
         new_value = map.get(cod)+new_value;
-        map.put(cod, new_value <0 ? 0 : new_value);
+        map.put(cod, Math.max(new_value,0));
     }
     public static void menu(Map<String,Integer> map) {
         Scanner sc = new Scanner(System.in);
@@ -46,7 +46,7 @@ public class Menu {
                     break;
                 case 4:
                     map.forEach((codigoProducto,cantidad) -> System.out.println("Codigo de producto: " + codigoProducto + "\n \t Cantidad: " + cantidad)); /* Este metodo empieza al parecer por el más reciente, poniendolo primero esto tiene pinta de que es
-                     mas util si quieres iterar una estructura como una lista o así y aplicar una funcion o así sobre los elementos que contiene el objeto o sobre el propio objeto o al menos esa es la impresion que me da aunque bueno, al no haber un criterio de como se tiene que mostrar pues me da igual
+                     más util si quieres iterar una estructura como una lista o así y aplicar una funcion o así sobre los elementos que contiene el objeto o sobre el propio objeto o al menos esa es la impresion que me da aunque bueno, al no haber un criterio de como se tiene que mostrar pues me da igual
                      También se puede usar entrySet para obtener todas las claves y recorrerlas con un for Each de los tipicos además la implementacion base de esto que creo que proviene de Iterable
                        se comporta por defecto haciendo eso internamente*/
                     break;
