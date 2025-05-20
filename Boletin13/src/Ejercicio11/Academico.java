ceb5125f3268a814d123cd603285993a8bd94550
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-05-20 11:59:16 +0200

    Añadiendo cosas

--- Boletin13/src/Ejercicio11/Academico.java
+++ Boletin13/src/Ejercicio11/Academico.java
@@ -1,9 +1,17 @@
 package Ejercicio11;
 
+/**
+ * Clase que define a un Academico de la RAE
+ */
 public class Academico implements Comparable<Academico>{
     String nome;
     int anoIngreso;
 
+    /**
+     * Generamos el objeto de la clase Academico con los datos requeridos
+     * @param nome El nombre del usuario
+     * @param anoIngreso el año en el que ingreso
+     */
     public Academico(String nome,int anoIngreso) {
         this.nome = nome;
         this.anoIngreso = anoIngreso;
--- Boletin13/src/Ejercicio11/Main.java
+++ Boletin13/src/Ejercicio11/Main.java
@@ -1,16 +1,30 @@
 package Ejercicio11;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Map<Character,Academico> academia = new HashMap<>();
-        Academico academico1 = new Academico("Juan de la rosa",1999);
+        Academico academico0 = new Academico("Abel del Sn",1900);
+        Academico academico1 = new Academico("Ana de la rosa",1999);
         Academico academico2 = new Academico("Jorge de la piedra",2001);
-        nuevoAcademico(academia,academico1,'D');
+        nuevoAcademico(academia,academico0,'Z');
+        if (nuevoAcademico(academia,academico1,'S')) {
+            System.out.println("Academico añadido");
+        }
         nuevoAcademico(academia,academico2,'R');
         System.out.println(academia);
+        List<Academico> academicos = new ArrayList<>(List.copyOf(academia.values()));
+        Collections.sort(academicos);
+        System.out.println(academicos);
     }
+
+    /**
+     * Añade un nuevo academico a un Map que es normalmente la academia
+     * @param academia la academia a la que pertenecen los academicos
+     * @param novo el nuevo academico a introducir
+     * @param letra la letra a la que esta asignada
+     * @return Verdadero si el academico fue añadido y falso si no fue añadido
+     */
     static boolean nuevoAcademico (Map<Character, Academico> academia, Academico novo, Character letra) {
         letra = Character.toUpperCase(letra);
         if (academia!=null && Character.isLetter(letra)) {
